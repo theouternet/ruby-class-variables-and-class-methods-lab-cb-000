692e02feffffa74768c6e23102af8d9dbd89b458
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-05 01:09:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, artist, genre)
+  def new(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
@@ -14,8 +14,6 @@ class Song
     @@genres << genre
   end
   
-  def new(name, artist, genre)
-  end
     
   def self.count
       @@count
