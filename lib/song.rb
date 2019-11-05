a81c602b1231edd28c4247de458f1715366a206a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-05 02:08:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
     if genre_count[genre]
-  genre_count << genre
+      genre_count[genre] += 1
   
 end
   return genre_count
