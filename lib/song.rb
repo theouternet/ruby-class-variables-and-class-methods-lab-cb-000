42c6a8ca17252f480fea7343cefef57072f3ebbc
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-05 01:01:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists
   end
   
   def new(name, artist, genre)
@@ -18,8 +19,8 @@ class Song
     
     def self.count
       @@count
-      @@artist
-      @@genre
+      @@artists
+      @@genres
     end
 
 
