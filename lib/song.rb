c7eed6cc39255cf0f282e9fa8b2ea6a386b69103
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-05 01:09:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def new(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
